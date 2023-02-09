c55052369f3b7c34619b944c716922bbdbd34315
Jo <jo@Jos-MacBook-Air.local>
2023-02-09 17:09:22 +0300

    Connect to mongodb

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv/config');
+
+const mongoose = require('mongoose');
+const app = require('./app');
+
+mongoose.connect(process.env.MONGODB_LOCAL, () => {
+    console.log("Connected to DB");
+})
+ 
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+}
+);
