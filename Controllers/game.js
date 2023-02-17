f089f93f437f089c348e8c10b53b365fe9889e13
Jo <jo@Jos-MacBook-Air.local>
2023-02-17 14:29:33 +0300

    Add game controller

---
+++ Controllers/game.js
@@ -0,0 +1,10 @@
+const Game = require('../Models/game');
+
+
+module.exports.createGame = async (req, res) => {
+    console.log('createGame');
+}
+
+module.exports.answerGameQuestion = async (req, res) => {
+    console.log('getGame');
+}
