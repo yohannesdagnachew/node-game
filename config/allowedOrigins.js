8767c138447a0a3081be50c6189c324db940d5bc
Zelalem <mekonnen.zelalem251@gmail.com>
2023-03-03 17:35:01 +0300

    Fix backend api url typo

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-	'https://lambent-maamoul-6d6af3.netlify.app/',
+	'https://lambent-maamoul-6d6af3.netlify.app',
 	'http://localhost:3000',
 	'http://localhost:3001',
 	'http://localhost:3002',
