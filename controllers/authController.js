99433452c352562b0b46360cb78c930f91c8353a
Zelalem <mekonnen.zelalem251@gmail.com>
2023-03-03 18:21:46 +0300

    Make the refresh token cookie secure=true

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,14 +16,14 @@ const cookiesOption = {
 			Date.now() + process.env.REFRESH_TOKEN_COOKIE_EXPIRES_IN * 60 * 1000
 		),
 		httpOnly: true,
-		secure: process.env.NODE_ENV === 'production',
+		secure: true,
 	},
 	access: {
 		maxAge: new Date(
 			Date.now() + process.env.ACCESS_TOKEN_COOKIE_EXPIRES_IN * 60 * 1000
 		),
 		httpOnly: false,
-		secure: process.env.NODE_ENV === 'production',
+		secure: true,
 	},
 };
 
