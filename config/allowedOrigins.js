32a00be43dbc095565f299611b1cfed03922775a
Zelalem Mekonnen <mekonnen.zelalem251@gmail.com>
2023-03-10 22:00:43 +0300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
 	'https://lambent-maamoul-6d6af3.netlify.app',
+	'https://meek-taiyaki-f7a602.netlify.app',
 	'http://localhost:3000',
 	'http://localhost:3001',
 	'http://localhost:3002',
